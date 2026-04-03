edf611be85636fe9d755d66eb777f70cb67adbd9
James Barrett <jbs.public.function@gmail.com>
2026-04-02 19:53:20 -0500

    Modify print statement for installer

--- install-junkdrawer.py
+++ install-junkdrawer.py
@@ -12,13 +12,24 @@ if os.name == 'nt':  # Windows
 else:                # macOS/Linux
     venv_python = os.path.join(venv_dir, 'bin', 'python')
 
+# Upgrade pip
+try:
+    subprocess.run(
+        [venv_python, "-m", "pip", "install", "--upgrade", "pip"],
+        check=True
+    )
+    print("Successfully upgraded pip...")
+except subprocess.CalledProcessError as e:
+    print(f"Installation failed: {e}")
+
+
 # 3. Install from requirements.txt
 try:
     subprocess.run(
         [venv_python, "-m", "pip", "install", "-r", "requirements.txt"],
         check=True
     )
-    print("Successfully installed requirements.")
+    print("Successfully installed requirements...")
 except subprocess.CalledProcessError as e:
     print(f"Installation failed: {e}")
 
@@ -27,7 +38,7 @@ try:
         [venv_python, "-m", "ipykernel", "install", "--user", "--name", "junkdrawer", "--display-name", "junkdrawer"],
         check=True
     )
-    print("Successfully installed kernelspec.")
+    print("Successfully installed kernelspec...")
 except subprocess.CalledProcessError as e:
     print(f"Installation failed: {e}")
 
